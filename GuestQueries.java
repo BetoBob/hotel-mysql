3baeeb519960cc2cbeff3ce6c83c02c0ab51f93a
BetoBob <rehensle@calpoly.edu>
2018-11-29 21:53:39 -0800

    query classes

---
+++ GuestQueries.java
@@ -0,0 +1,13 @@
+class GuestQueries
+{
+    // OR-1 Occupancy overview
+
+    // OR-2 Revenue
+
+    // OR-3 Reservations
+
+    // OR-4 Rooms
+
+    // OR-5 Detailed reservation information
+    
+}
